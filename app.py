9a0735a490de418e5e9d0f5e5cb6dc86f0eb9a49
ekuin0x <salhimouiz1@gmail.com>
2023-12-30 00:15:27 +0100

    ljn

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask  
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 app = Flask(__name__)
 
 @app.route("/")
 def index() :
-    browser = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    browser = webdriver.Chrome(chrome_options=chrome_options)
     browser.get("https://www.google.com")
     return "Hello World !!"
 
